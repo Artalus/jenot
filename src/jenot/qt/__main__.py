f42cb012525bff741110e590cf557925d82e118f
Artalus <artalus-mail@yandex.ru>
2021-10-27 15:37:59 +0300

    feat: log unhandled exceptions

--- src/jenot/qt/__main__.py
+++ src/jenot/qt/__main__.py
@@ -1,11 +1,10 @@
 import sys
 import signal
+import traceback
+from typing import Any, Type
 
 from PyQt5.QtWidgets import (
     QApplication,
-)
-from PyQt5.QtGui import (
-    QIcon,
 )
 
 from jenot.qt.main import MainWidget
@@ -13,8 +12,16 @@ from jenot.qt.tray import JenotTray
 import jenot.log as log
 
 
-def _main() -> int:
-    log.trace("main")
+def exception_hook(exc_type: Type[BaseException], exc_value: BaseException, exc_tb: Any) -> None:
+    # TODO: prints `NoneType: None` at the end of trace for some reason
+    tb = ''.join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    log.exception(f'unhandled exception:\n{tb}')
+    QApplication.exit(-1)
+
+
+def main() -> int:
+    sys.excepthook = exception_hook
+    log.trace(f'{sys.argv[0]} starting')
     # to kill pyqt with ^C https://stackoverflow.com/a/5160720/5232529
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
@@ -26,13 +33,6 @@ def _main() -> int:
 
     trayIcon.show()
     return app.exec()
-
-def main() -> int:
-    try:
-        return _main()
-    except Exception as e:
-        log.exception("exce")
-        return -1
 
 
 if __name__ == '__main__':
