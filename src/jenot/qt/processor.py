4141d10bf40e24f34e94ff62f8c7853a3edcf259
Artalus <artalus-mail@yandex.ru>
2022-07-22 22:44:31 +0300

    wip: i do not know what i meant with this change

--- src/jenot/qt/processor.py
+++ src/jenot/qt/processor.py
@@ -1,3 +1,5 @@
+import time
+
 from PyQt5.QtCore import (
     QObject,
     QThread,
@@ -5,6 +7,7 @@ from PyQt5.QtCore import (
 )
 
 from jenot import run
+from jenot import log
 
 class Processor(QObject):
     finished = pyqtSignal(int, str)
@@ -25,10 +28,19 @@ class Processor(QObject):
 
         self._thread = thread
 
-
     def _run(self) -> None:
-        result = run(self.jenkins_base_url, self.user, self.token, self.build_part)
-        self.finished.emit(*result)
+        for attempt in range(1,1+3):
+            try:
+                result = run(self.jenkins_base_url, self.user, self.token, self.build_part)
+                self.finished.emit(*result)
+                return
+            except Exception as e:
+                log.exception(f'Watch attempt {attempt}, exception happened: {e}')
+                log.info('sleeping for 5 sec before retry...')
+                time.sleep(5)
+        else:
+            log.error('Watch failed after 3 attempts')
+            self.finished.emit(1, self.build_part)
 
     def start(self) -> None:
         self._thread.start()
